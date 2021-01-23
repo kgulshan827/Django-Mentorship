82a0f884a6d347bac7e62184dd1411407b88f1cf
kgulshan827 <kgulshan827@gmail.com>
2021-01-23 15:22:31 +0530

    adding all code

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticleForm
-
+from django.contrib.auth.decorators import login_required
 def articleList(request):
     articles = Articles.objects.all().order_by('date')
     return render(request, 'articleList.html', { 'articles': articles })
-
+@login_required(login_url="/accounts/login")
 def create(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST,request.FILES)
